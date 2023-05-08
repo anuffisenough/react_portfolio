55e62f1bde770558074156d554e708e0016efde1
anuffisenough <uffmanch@gmail.com>
2023-05-08 12:06:46 -0600

    adds project details to project.js

---
+++ portfolio/src/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+    {
+        name: {
+            title: 'KaraokeDokie',
+    },
+        image: {
+            thumbnail: '',
+    },
+        deployed_link: {
+            link: 'https://charles-ram.github.io/karaoke-dokie-project/',
+    },
+        github_repository: {
+            link: 'https://github.com/charles-ram/karaoke-dokie-project',
+    },
+},
+{
+    name: {
+        title: 'ParkingBuddy',
+},
+    image: {
+        thumbnail: '',
+},
+    deployed_link: {
+        link: 'https://serene-cove-30368.herokuapp.com/',
+},
+    github_repository: {
+        link: 'https://github.com/SMU-CodingBootcamp-Project2/ParkingBuddy',
+},
+},
+{
+    name: {
+        title: 'TBD',
+},
+    image: {
+        thumbnail: '',
+},
+    deployed_link: {
+        link: '',
+},
+    github_repository: {
+        link: '',
+},
+},
+];
+
+export default projects;

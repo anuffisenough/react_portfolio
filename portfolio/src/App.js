707ad4d84d649e3e62ba626865a9a93dacc16444
anuffisenough <uffmanch@gmail.com>
2023-05-08 11:42:39 -0600

    sets up file structure and initial files

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+
+function App() {
+  return (
+  <div>
+      <Header />
+      <Navigation />
+      <Project />
+      <Footer />
+  </div>
+  );
+}
+
+export default App;

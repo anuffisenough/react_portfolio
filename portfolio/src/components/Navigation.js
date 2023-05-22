9d70a8b8d9276b35f21c3683aa617185e5d17548
anuffisenough <uffmanch@gmail.com>
2023-05-22 09:34:57 -0600

    fixes sections according to README

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -30,7 +30,7 @@ function Navigation({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
         >
-          About Christopher
+          About Me
         </a>
       </li>
       <li className="nav-item">
@@ -40,7 +40,7 @@ function Navigation({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
         >
-          Projects
+          Portfolio
         </a>
       </li>
       <li className="nav-item">

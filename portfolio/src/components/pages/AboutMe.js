94d7514b593368724ff463e536307c681e098296
anuffisenough <uffmanch@gmail.com>
2023-05-22 09:53:54 -0600

    adds photo and updates projects

--- portfolio/src/components/pages/AboutMe.js
+++ portfolio/src/components/pages/AboutMe.js
@@ -4,8 +4,14 @@ export default function AboutMe() {
   return (
     <div>
       <h1>About Christopher</h1>
-      <p>
+      <div class="flex-hero">
 
+    <div className="image" src="Assets/images/Professional_Photo.png"></div>
+    </div>
+      <p>
+      There is no single aspect of my career as a collegiate coach that has brought me more pride and satisfaction than helping my student-athletes grow, improve, and succeed, in competition, in their studies, and in life after graduation. It is in this role that I was able to fully realize my potential as a mentor and advisor. I look forward to the chance to immerse myself in a learning culture where common goals and philosophies are shared among individuals at all levels.
+
+    My personal educational journey has provided me with deeper insight into the unique considerations of adult learners, culminating in my Master’s degree in Education with a concentration of Leadership in Adult and Organizational Learning. This hybrid program with a focus on educational culture and hierarchy has helped me to better understand my value within the organizational structures of higher education. Additionally, the leadership component has highlighted my strengths in the areas of connectedness, responsibility, and communication.
       </p>
     </div>
   );

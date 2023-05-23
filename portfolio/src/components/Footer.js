cb83225a0e63bc50861f8b24ff77d91742ca7abb
anuffisenough <uffmanch@gmail.com>
2023-05-22 19:27:08 -0600

    adds links to footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
+
+    <div>
+    <a href='https://github.com/anuffisenough?tab=repositories'>GitHub</a><a href='https://www.linkedin.com/in/christopher-uffman-a62575130/'>LinkedIn</a>
+    <a href='https://twitter.com/UffleShuffle'>Twitter</a>
+    </div>
+    
   );
 }
 

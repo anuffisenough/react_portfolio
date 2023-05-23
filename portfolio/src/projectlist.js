6eb56700ce68b3a0e76950c4d1441081b382b068
anuffisenough <uffmanch@gmail.com>
2023-05-22 19:19:31 -0600

    adds links to project list

--- portfolio/src/projectlist.js
+++ portfolio/src/projectlist.js
@@ -7,15 +7,15 @@ const Projects = [
     },
     {
         title: 'ParkingBuddy',
-        thumbnail: '',
+        thumbnail: 'ParkingBuddyHPScreenShot.png',
         deployed_link: 'https://charles-ram.github.io/karaoke-dokie-project/',
         github_repository: 'https://github.com/charles-ram/karaoke-dokie-project',
     },
     {
         title: 'Cardify',
-        thumbnail: '',
-        deployed_link: '',
-        github_repository: '',
+        thumbnail: 'CardifyHomePage.png',
+        deployed_link: 'https://floating-brushlands-04245.herokuapp.com/',
+        github_repository: 'https://github.com/SMU-Bootcamp-Project3/Cardify',
     },
 ];
 
